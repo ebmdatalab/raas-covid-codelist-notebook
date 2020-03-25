2cb8b4cc7ec7ada92afd681fc345c5d468040a6f
Brian <brianmkcebm@gmail.com>
2020-03-25 11:54:41 +0000

    first commit of RAAS codelist

---
+++ notebooks/diffable_python/raas.codelist.py
@@ -0,0 +1,43 @@
+# ---
+# jupyter:
+#   jupytext:
+#     cell_metadata_filter: all
+#     notebook_metadata_filter: all,-language_info
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.3.3
+#   kernelspec:
+#     display_name: Python 3
+#     language: python
+#     name: python3
+# ---
+
+from ebmdatalab import bq
+import os
+
+# +
+sql = '''WITH bnf_codes AS (
+  SELECT bnf_code FROM hscic.presentation WHERE 
+  bnf_code LIKE '020505%' OR #bnf section rass meds
+  bnf_code LIKE '0206020Z0%' ##valsartan amlodipine combo
+  )
+
+SELECT "vmp" AS type, id, bnf_code, nm
+FROM dmd.vmp
+WHERE bnf_code IN (SELECT * FROM bnf_codes)
+
+UNION ALL
+
+SELECT "amp" AS type, id, bnf_code, descr
+FROM dmd.amp
+WHERE bnf_code IN (SELECT * FROM bnf_codes)
+
+ORDER BY type, bnf_code, id'''
+
+raas_codelist = bq.cached_read(sql, csv_path=os.path.join('..','data','raas_codelist.csv'))
+raas_codelist.head(10)
+# -
+
+
